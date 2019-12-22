382707d913cbbdca3f1af7c2eadf90f6d2e25ea4
EC2 Default User <ec2-user@ip-172-31-87-109.ec2.internal>
2019-12-22 14:47:13 +0000

    take out enumerate

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -6,18 +6,14 @@ import os
 def kmeans(inputPath,K) :
 
     transactions = [] # list of all points wihout parse
-    frequentKItems = {}
-    totalLineCount = 0
 
     with open(inputPath) as f:
          for line in f:
-            totalLineCount = totalLineCount + 1
             line = line.rstrip()
             data = re.split(',', line)
             transactions.append(data)
-            print ("data --> ", data)
-            for category in data:
-                print ("cat -->", category)
-                    
+    
+    numPoints = len(transactions)
+    
                     
 kmeans("places.txt",3);
