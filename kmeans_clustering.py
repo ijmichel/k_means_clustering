6714c1527f5ff2f594270cffd2aba939375a92bc
EC2 Default User <ec2-user@ip-172-31-87-109.ec2.internal>
2019-12-23 19:43:01 +0000

    Get random 3 points

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -5,18 +5,22 @@ import os
 import numpy as np
 
 def kmeans(inputPath,K) :
-
+    
     transactions = [] # list of all points wihout parse
 
     with open(inputPath) as f:
          for line in f:
             line = line.rstrip()
             data = re.split(',', line)
-            print ("data -->",data)
+            # print ("data -->",data)
             transactions.append(data)
     
     numPoints = len(transactions)
     
+    x = np.array(transactions)
+    
+    indices = np.random.choice(x.shape[0], 3, replace=False)
+    print ("Choices 3--> ", indices)
 
                     
 kmeans("places.txt",3);
