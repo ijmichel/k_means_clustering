8fa5aff1ebf9bafaee69eb1dd25524eca012b3a4
EC2 Default User <ec2-user@ip-172-31-87-109.ec2.internal>
2019-12-27 15:00:11 +0000

    Find nearest Centroid

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -2,6 +2,7 @@
 
 import re
 import os
+import math
 import numpy as np
 
 def kmeans(inputPath,K) :
@@ -19,8 +20,32 @@ def kmeans(inputPath,K) :
     
     x = np.array(transactions)
     
-    indices = np.random.choice(x.shape[0], 3, replace=False)
-    print ("Choices 3--> ", indices)
+    indices = np.random.choice(x.shape[0], K, replace=False)
+    print ("Choices K--> ", indices)
+    
+    currentCentroids = []
+    for n in range(K):
+        print ("X --> ",n)
+        currentCentroids.append(transactions[indices[n]])
+        print ("Ponit X--> ", n, " : ", currentCentroids[n])
+        
+    pointCentroidIndex = {}
 
+    for i,aPoint in enumerate(x):
+        print ("aPoint", aPoint)
+        leastDist = 99999
+        Px = aPoint[0]
+        Py = aPoint[1]
+        for j,aCentroid in enumerate(currentCentroids):
+            Cx = aCentroid[0]
+            Cy = aCentroid[1]
+            print (Px , ":",Py,":",Cx,":",Cy)
+            currDistance = math.sqrt((float(Px) - float(Cx))**2 + (float(Py) - float(Cy))**2 )
+            if currDistance < leastDist:
+                leastDist = currDistance
+                print ("i-->",i)
+                pointCentroidIndex[i] = j
+                print("point ", i, " Centroid --> ", currentCentroids[j])
+                    
                     
 kmeans("places.txt",3);
